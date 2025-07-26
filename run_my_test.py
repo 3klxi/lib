75f7e39b7caac686a48c5bb1c045f8ec06537d11
3klxi <karenxindongle@126.com>
2025-07-27 01:24:33 +0800

    [factor] 测试了 Pycharm 的 git 管理

--- run_my_test.py
+++ run_my_test.py
@@ -3,3 +3,11 @@ import numpy as np
 np.random.seed(42)
 
 print(np.random.randn(2, 3))
+
+import numpy as np
+np.random.seed(42)
+
+data1 = np.array([2,3,4])
+data2 = np.array([2,3,4])
+data3 = np.vstack((data1,data2))
+print(data3)
